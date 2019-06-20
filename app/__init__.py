a9ff45986ae16141813be878ae3b4bbf7c46808c
Sergey Ilchenko <isv10k@gmail.com>
2019-06-20 18:29:04 +0500

    endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
 
+from flask_restful import Api
+
 app = Flask(__name__)
 
-from app import views
+api = Api(app)
+
+from app import views, resources
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.AllUsers, '/users')
+api.add_resource(resources.SecretResource, '/secret')
