5789c087356d695346c049621dbe0ec60da9bb7d
Sergey Ilchenko <isv10k@gmail.com>
2019-06-20 18:59:53 +0500

    login/registration

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 
 from flask_restful import Api
 
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
 
 api = Api(app)
+db = SQLAlchemy(app)
 
 from app import views, resources
 
+# Endpoints
 api.add_resource(resources.UserRegistration, '/registration')
 api.add_resource(resources.UserLogin, '/login')
 api.add_resource(resources.UserLogoutAccess, '/logout/access')
@@ -15,3 +20,14 @@ api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
 api.add_resource(resources.TokenRefresh, '/token/refresh')
 api.add_resource(resources.AllUsers, '/users')
 api.add_resource(resources.SecretResource, '/secret')
+
+# DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'pass'
+
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
--- app/resources.py
+++ app/resources.py
@@ -1,14 +1,42 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from app.models import UserModel
+
+parser = reqparse.RequestParser()
+parser.add_argument('username', help = 'This field cannot be blank', required = True)
+parser.add_argument('password', help = 'This field cannot be blank', required = True)
 
 
 class UserRegistration(Resource):
     def post(self):
-        return {'message': 'User registration'}
+        data = parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {'message': 'User {} already exists'.format(data['username'])}
+
+        new_user = UserModel(
+            username = data['username'],
+            password = data['password']
+        )
+        try:
+            new_user.save_to_db()
+            return {
+                'message': 'User {} was created'.format( data['username'])
+            }
+        except:
+            return {'message': 'Something went wrong'}, 500
 
 
 class UserLogin(Resource):
     def post(self):
-        return {'message': 'User login'}
+        data = parser.parse_args()
+        current_user = UserModel.find_by_username(data['username'])
+        if not current_user:
+            return {'message': 'User {} doesn\'t exist'.format(data['username'])}
+
+        if data['password'] == current_user.password:
+            return {'message': 'Logged in as {}'.format(current_user.username)}
+        else:
+            return {'message': 'Wrong credentials'}
 
 
 class UserLogoutAccess(Resource):
@@ -28,10 +56,10 @@ class TokenRefresh(Resource):
 
 class AllUsers(Resource):
     def get(self):
-        return {'message': 'List of users'}
+        return UserModel.return_all()
 
     def delete(self):
-        return {'message': 'Delete all users'}
+        return UserModel.delete_all()
 
 
 class SecretResource(Resource):
